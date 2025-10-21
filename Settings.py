7ce5b2cea46a283ff821ef4777f65c7a8777e51e
Teaoko <taylortalbot315@gmail.com>
2025-10-20 17:42:38 -0700

    Initial commit

---
+++ Settings.py
@@ -0,0 +1,25 @@
+#These will all be the same
+#per value will change the value, per level will change the level
+#xxps: xp per second, and xp next will be how much it adds each time for the next level
+#Class settings
+
+#Rebirth settings
+
+#Ascencion settings
+
+#Stregnth settings
+
+#Endurance settings
+#EPV, EPL: endurance per value/level 
+endurance_list = {
+    {'name': "pushup", 'EPV': 1, 'EPL': 1, 'xp_needed': 0, 'xpps': 0, 'xp_next': 20},   
+}
+
+#Agility settings
+
+#Speed settings
+
+#Meditation settings
+
+#Increase settings
+#Go to plan.txt for now
